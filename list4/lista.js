21b632668b9214d3a78a50684f3570a9e4104f3e
brayanzinn <162717265+Dynamiketengu@users.noreply.github.com>
2024-06-12 08:40:21 -0400

    Add files via upload

--- list4/lista.js
+++ list4/lista.js
@@ -1,7 +1,17 @@
-function clientes(nome, idade, lista){
-    const clientes = {
-        nome: "nicolas",
-        idade: 16,
-        lista: "laranja", 
+let cliente = [""]
+function cliente(nome, idade, lista){
+    const cliente = {
+        nome: nome,
+        idade: idade,
+        lista: lista,
     };
+    cliente.push(novoCliente);
+    console.log(`cliente adicionado: ${nome}`)
 };
+
+function listarClientes(){
+    console.log("lista de Clientes:");
+    listarClientes.forEach((cliente, index){
+        console.log(`#${index + 1} - ${cliente.nome} - ${cliente.idade} - ${cliente.lista}`)
+    });
+}
